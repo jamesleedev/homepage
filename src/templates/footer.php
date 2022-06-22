c7a0898a4cf747038da187b42760f0d522593afd
James Lee <james@jamesl.dev>
2022-06-22 18:24:45 +0100

    Added php to get last modified day.

--- src/templates/footer.php
+++ src/templates/footer.php
@@ -1,6 +1,20 @@
+<?php
+    function getLastModified() {
+        $filemap = [
+            '/' => '../public/index.php',
+            '/contact' => '../public/contact.php',
+            '/projects' => '../public/projects.php'
+        ];
+        $request = $_SERVER['REQUEST_URI'];
+        if (array_key_exists($request, $filemap) ) {
+            return "Last updated " . date("d F Y", filemtime($filemap[$request]));
+        }
+    }
+?>
+
 <footer class="container">
     <div class="content">
         <p>&copy; James Lee <?php echo date("Y"); ?></p>
-        <p>Last updated 5 April 2022</p>
+        <p><?= getLastModified(); ?></p>
     </div>
 </footer>
