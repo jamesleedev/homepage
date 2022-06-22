0cc5c53476bbc1202d912a8f6f836f59f85faf52
James Lee <james@jamesl.dev>
2022-06-22 18:46:06 +0100

    Updated homepage content.

--- public/index.php
+++ public/index.php
@@ -32,11 +32,21 @@
     <section class="content">
         <h1>Who am I?</h1>
         <p>
-            I'm a front end developer at <a href="https://www.accointing.com" target="_blank" rel="noreferrer">ACCOINTING.com</a> based in Bristol and currently working full time.
-            I've been using Linux and coding recreationally for about 7 years, and I am experienced in Python and PHP, working with WordPress specifically.
+            I'm a front end developer with React/Next.js at <a href="https://www.accointing.com" target="_blank" rel="noreferrer">ACCOINTING.com</a> based near Bristol and currently working full time.
+            I am also a backend developer at <a href="https://serpex.co.uk">Serpex Studios</a>. I'm not currently looking for new opportunities but you can drop me a message anytime using my contact details.
         </p>
         <p>
-            I am also a backend developer at <a href="https://serpex.co.uk">Serpex Studios</a>.
+            I've been using Linux and coding recreationally for about 7 years, and I am experienced in Python and PHP, working with WordPress specifically.
+            Currently dual booting Fedora and Windows, with my distro history being Ubuntu then straight to Fedora as around 18.04 everything broke for me, and I run KDE at the moment, but was a long time MATE user.
+        </p>
+        <p>
+            In terms of tools, I use Notion/Google Keep for note taking, and Trello/Jira for personal/work task management. I code pretty much exclusively in JetBrains IDEs, with Emacs/VSCode as a text editor when needed.
+        </p>
+        <p>
+            Innis & Gunn and Laphroaig 10 are probably my favourite drinks at the moment.
+        </p>
+        <p>
+            Even though I'm a front end dev, I have literally no design skills. I am somewhat opinionated about front end development in that unnecessary complexity should always be avoided.
         </p>
         <p>
             My PGP key can be found in <a href="https://jamesl.dev/pgp.txt">/pgp.txt</a>
