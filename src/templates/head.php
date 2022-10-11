491bb01c04f7a2e6fa4e3efa14c06c47434d149b
James Lee <james@jamesl.dev>
2022-10-11 02:29:49 +0100

    Updated img paths.

--- src/templates/head.php
+++ src/templates/head.php
@@ -3,15 +3,15 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;700&display=swap" rel="stylesheet" media="print" onload="this.media='all'">
-<link rel="stylesheet" href="css/styles.css">
+<link rel="stylesheet" href="/css/styles.css">
 <meta property="og:type" content="website">
 <meta property="og:locale" content="en_GB">
-<meta property="og:image" content="https://www.jamesl.dev/img/og-image.png">
+<meta property="og:image" content="https://www.jamesl.dev/assets/img/og-image.png">
 <meta property="og:image:type" content="image/png">
 <meta property="og:image:width" content="1200">
 <meta property="og:image:height" content="630">
 <meta property="og:image:alt" content="jamesl.dev">
-<meta property="og:image" content="https://www.jamesl.dev/img/og-square.png">
+<meta property="og:image" content="https://www.jamesl.dev/assets/img/og-square.png">
 <meta property="og:image:type" content="image/png">
 <meta property="og:image:width" content="1200">
 <meta property="og:image:height" content="1200">
@@ -19,5 +19,5 @@
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:site" content="@jamesldev">
 <meta name="twitter:creator" content="@jamesldev">
-<meta name="twitter:image" content="https://www.jamesl.dev/img/og-image.png">
+<meta name="twitter:image" content="https://www.jamesl.dev/assets/img/og-image.png">
 <meta name="twitter:image:alt" content="jamesl.dev">
